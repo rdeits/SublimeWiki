0c4fea30da4c0bbc538fe1b98d610f52ed5592d6
Robin Deits <robin.deits@gmail.com>
2013-03-05 02:59:53 -0500

    support longer wiki names

--- wikilink.py
+++ wikilink.py
@@ -10,8 +10,9 @@ class WikiLinkCommand(sublime_plugin.TextCommand):
         #find the cursor
         location = self.view.sel()[0]
 
+        self.view.run_command("expand_selection", {"to": "brackets"})
         #find the word under the cursor
-        word = self.view.substr(self.view.word(location.a)).replace("*", "")
+        # word = self.view.substr(location).replace("*", "")
         scope = self.view.substr(self.view.extract_scope(location.a)).replace("*", "")
 
         if "link.external.Wiki" in self.view.scope_name(location.a):
@@ -20,7 +21,7 @@ class WikiLinkCommand(sublime_plugin.TextCommand):
 
         elif "markup.underline.link.image.markdown" in self.view.scope_name(location.a):
             sublime.status_message("try to open " + scope)
-            sublime.active_window().run_command('open_url', {"url": "file://" + directory + "/" + scope})
+            sublime.active_window().run_command('open_url', {"url": "file://" + directory + slash + scope})
 
         elif "link.email.Wiki" in self.view.scope_name(location.a):
                 sublime.status_message("try to mail " + scope)
@@ -29,7 +30,7 @@ class WikiLinkCommand(sublime_plugin.TextCommand):
             #okay, we're good. Keep on keepin' on.
 
             #compile the full file name and path.
-
+            word = scope.replace('[', '').replace(']', '')
             new_file = directory+slash+word+".md"
             #debug section: uncomment to write to the console
             # print "Location: %d" % location.a
